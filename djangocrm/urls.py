b3e7ab7aa285d6e57095a8caf27aeedca6d82261
Haris Ali <aliharris727@gmail.com>
2024-05-04 12:42:49 +0500

    Comment authentication url

--- djangocrm/urls.py
+++ djangocrm/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('webapp/', include("webapp.urls")),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
